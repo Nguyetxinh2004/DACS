bc591a38084b41b0ad7a395057888b39a713febd
Nguyetxinh2004 <624nguyetminh@gmail.com>
2025-03-30 11:23:34 +0700

    123

--- js/admin.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function () {
-    var navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-})
